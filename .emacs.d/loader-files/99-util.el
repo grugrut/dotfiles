dfbd5ec353ab749ed5b150703728f83608a4f37b
grugrut <grugruglut+github@gmail.com>
2017-11-18 08:47:15 +0900

    suspendの暴発を防ぐ

--- .emacs.d/loader-files/99-util.el
+++ .emacs.d/loader-files/99-util.el
@@ -4,3 +4,10 @@
   (let ((text (format "%s L%d" (buffer-name) (line-number-at-pos))))
     (message text)
     (kill-new text)))
+
+;;; WSLでsuspendが暴発すると復帰できない？ ので確認する
+(defun my/confirm-suspend ()
+  ""
+  (unless (y-or-n-p "Really suspend? ")
+    (error "Suspend canceld")))
+(add-hook 'suspend-hook 'my/confirm-suspend)
