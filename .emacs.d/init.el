36d3ca276d1258df9164e02f5b7ad1b9e260096a
grugrut <grugruglut+github@gmail.com>
2018-12-11 00:04:17 +0900

    use manoj-dark theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -285,50 +285,42 @@
   :config
   (setq popwin:popup-window-position 'bottom))
 
-(use-package manoj-dark
-  :disabled t
-  :init
-  (load-theme 'manoj-dark))
-
-(use-package atom-one-dark-theme
-  :ensure
-  :config
-  (load-theme 'atom-one-dark t))
+(load-theme 'manoj-dark)
 
 ;; 本当はひとつのuse-packageにまとめたいが、spaceline-define-segmentがマクロ展開できないため、先にrequireする
 (use-package spaceline-config
   :ensure spaceline
   :config
 
-(spaceline-define-segment my/buffer-modified
-  "A modified segment"
-  (let* ((config-alist '(("*" all-the-icons-faicon-family all-the-icons-faicon "chain-broken" :v-adjust 0.0)
-                         ("-" all-the-icons-faicon-family all-the-icons-faicon "link" :v-adjust 0.0)
-                         ("%" all-the-icons-faicon-family all-the-icons-faicon "lock" :v-adjust 0.0)))
-         (result (cdr (assoc (format-mode-line "%*") config-alist))))
-    (propertize (format "%s" (apply (cadr result) (cddr result))) 'face `(:family ,(funcall (car result)) :inherit))))
+  (spaceline-define-segment my/buffer-modified
+    "A modified segment"
+    (let* ((config-alist '(("*" all-the-icons-faicon-family all-the-icons-faicon "chain-broken" :v-adjust 0.0)
+                           ("-" all-the-icons-faicon-family all-the-icons-faicon "link" :v-adjust 0.0)
+                           ("%" all-the-icons-faicon-family all-the-icons-faicon "lock" :v-adjust 0.0)))
+           (result (cdr (assoc (format-mode-line "%*") config-alist))))
+      (propertize (format "%s" (apply (cadr result) (cddr result))) 'face `(:family ,(funcall (car result)) :inherit))))
 
-(spaceline-define-segment my/major-mode
-  "A major-mode segment"
-  (let ((icon (all-the-icons-icon-for-buffer)))
-    (unless (symbolp icon)
-      (propertize icon
-                  'mouse-face 'mode-line-highlight
-                  'help-echo (format "Major mode: `%s'\n\ mouse-1: Display major mode menu\n\ mouse-2: Show help for major mode\n\ mouse-3: Toggle minor modes" major-mode)
-                  'local-map (let ((map (make-sparse-keymap)))
-                               (define-key map [mode-line down-mouse-1]
-                                 `(menu-item ,(purecopy "Menu Bar") ignore
-                                             :filter (lambda (_) (mouse-menu-major-mode-map))))
-                               (define-key map [mode-line mouse-2] 'describe-mode)
-                               (define-key map [mode-line down-mouse-3] mode-line-mode-menu)
-                               map)
-                  'face `(:family ,(all-the-icons-icon-family-for-buffer) :inherit)))))
+  (spaceline-define-segment my/major-mode
+    "A major-mode segment"
+    (let ((icon (all-the-icons-icon-for-buffer)))
+      (unless (symbolp icon)
+        (propertize icon
+                    'mouse-face 'mode-line-highlight
+                    'help-echo (format "Major mode: `%s'\n\ mouse-1: Display major mode menu\n\ mouse-2: Show help for major mode\n\ mouse-3: Toggle minor modes" major-mode)
+                    'local-map (let ((map (make-sparse-keymap)))
+                                 (define-key map [mode-line down-mouse-1]
+                                   `(menu-item ,(purecopy "Menu Bar") ignore
+                                               :filter (lambda (_) (mouse-menu-major-mode-map))))
+                                 (define-key map [mode-line mouse-2] 'describe-mode)
+                                 (define-key map [mode-line down-mouse-3] mode-line-mode-menu)
+                                 map)
+                    'face `(:family ,(all-the-icons-icon-family-for-buffer) :inherit)))))
 
-(spaceline-define-segment my/time
-  "A datetime segment"
-  (propertize (format-time-string "%m/%d %H:%M") 'display '(raise 0.1)
-              'help-echo (format-time-string "%c"))
-  :tight t :enabled t))
+  (spaceline-define-segment my/time
+    "A datetime segment"
+    (propertize (format-time-string "%m/%d %H:%M") 'display '(raise 0.1)
+                'help-echo (format-time-string "%c"))
+    :tight t :enabled t))
 
 (use-package spaceline-config
   :config
