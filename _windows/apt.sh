5ab266642a8ddb2c987234306a664bce1b937998
grugrut <grugruglut+github@gmail.com>
2019-07-03 06:46:15 +0900

    Add packages for build emacs

--- _windows/apt.sh
+++ _windows/apt.sh
@@ -3,8 +3,21 @@
 INSTALL_PACKAGES="
 build-essential
 fonts-migmix
+autoconf
+texi2html
+texinfo
+libgtk-3-dev
+libwebkit2gtk-4.0-dev
+libxpm-dev
+libjpeg-dev
+libgif-dev
+libtiff-dev
+gnutls-bin
+libgnutls28-dev
+libtinfo-dev
+libncurses5-dev
+mailutils
 "
-
 
 apt update
 apt upgrade -y 
--- _windows/setup.sh
+++ _windows/setup.sh
@@ -10,6 +10,10 @@ mkdir -p src/github.com/grugrut
 sudo ${NOW}/apt.sh
 
 ## golang setting
-go get -u github.com/golang/lint/golint
+go get -u golang.org/x/lint/golint
 go get -u golang.org/x/tools/cmd/gopls
 
+## tools
+go get -u github.com/motemen/ghq
+git config --global ghq.root ~/src
+pip install percol
