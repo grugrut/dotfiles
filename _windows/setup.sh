58b589bb1b5eb9c3f43b4876c26d8289be7d348b
grugrut <grugruglut+github@gmail.com>
2019-07-02 23:55:44 +0900

    update

--- _windows/setup.sh
+++ _windows/setup.sh
@@ -5,4 +5,11 @@ cd ~
 ## make directory scaffold
 mkdir -p bin
 mkdir -p src/github.com/grugrut
+
+## install packages
 sudo ${NOW}/apt.sh
+
+## golang setting
+go get -u github.com/golang/lint/golint
+go get -u golang.org/x/tools/cmd/gopls
+
