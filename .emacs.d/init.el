6bb90074f25052d74c8b55013ae999ea30cd757d
grugrut <grugruglut+github@gmail.com>
2020-01-17 08:22:37 +0900

    invalid flymake on lsp-ui mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -646,6 +646,7 @@ _G_
   (lsp-mode-hook . lsp-ui-mode)
   :custom
   (lsp-ui-sideline-enable . nil)
+  (lsp-prefer-flymake . nil)
   :config
   (define-key lsp-ui-mode-map [remap xref-find-definitions] 'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] 'lsp-ui-peek-find-references)
