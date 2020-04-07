528f3c36fa04f83b0d52ca94891fdf6586ad26a9
grugrut <grugruglut+github@gmail.com>
2020-04-07 23:42:47 +0900

    Update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -334,79 +334,82 @@
   :config
   (global-auto-revert-mode t))
 
-(leaf google-this
-  :ensure t
-  :leaf-defer t
-  :bind (("M-s g" . google-this-noconfirm)))
-
-(leaf anzu
-  :ensure t
-  :bind
-  (("M-%" . anzu-query-replace))
+(leaf search-functions
   :config
-  (global-anzu-mode +1)
+  (leaf google-this
+    :ensure t
+    :leaf-defer t
+    :bind (("M-s g" . google-this-noconfirm)))
+
+  (leaf anzu
+    :ensure t
+    :bind
+    (("M-%" . anzu-query-replace))
+    :config
+    (global-anzu-mode +1)
+    )
+
+  (leaf migemo
+    :ensure t
+    :require t
+    :custom
+    (migemo-command . "cmigemo")
+    (migemo-options . '("-q" "--emacs"))
+    (migemo-dictionary . "/usr/share/cmigemo/utf-8/migemo-dict")
+    (migemo-user-dictionary . nil)
+    (migemo-regex-dictionary . nil)
+    (migemo-coding-system . 'utf-8-unix)
+    :config
+    (migemo-init))
+
+  (leaf ripgrep
+    :ensure t
+    :leaf-defer t
+    :bind (("M-s r" . ripgrep-regexp))
+    :config
+    (setq ripgrep-arguments '("-S")))
+
+  ;; 大文字・小文字を区別しないでサーチ（有効：t、無効：nil）
+  (setq case-fold-search nil)
+
+  ;; インクリメント検索時に縦スクロールを有効化（有効：t、無効：nil）
+  (setq isearch-allow-scroll nil)
+
+  ;; C-dで検索文字列を一文字削除
+  (define-key isearch-mode-map (kbd "C-d") 'isearch-delete-char)
+
+  ;; C-yで検索文字列にヤンク貼り付け
+  (define-key isearch-mode-map (kbd "C-y") 'isearch-yank-kill)
+
+  ;; C-eで検索文字列を編集
+  (define-key isearch-mode-map (kbd "C-e") 'isearch-edit-string)
+
+  ;; Tabで検索文字列を補完
+  (define-key isearch-mode-map (kbd "TAB") 'isearch-yank-word)
+
+  ;; C-gで検索を終了
+  (define-key isearch-mode-map (kbd "C-g")
+    '(lambda()(interactive) (isearch-done)))
+
+  ;; 日本語の検索文字列をミニバッファに表示
+  (define-key isearch-mode-map (kbd "<compend>")
+    '(lambda() (interactive) (isearch-update)))
+  (define-key isearch-mode-map (kbd "<kanji>")
+    'isearch-toggle-input-method)
+  (add-hook
+   'isearch-mode-hook
+   '(lambda() (setq w32-ime-composition-window (minibuffer-window)))
+   )
+  (add-hook
+   'isearch-mode-end-hook
+   '(lambda() (setq w32-ime-composition-window nil))
+   )
+
+  ;; minibufferのアクティブ時、IMEを無効化
+  (add-hook 'minibuffer-setup-hook
+            (lambda ()
+              (deactivate-input-method)))
   )
-
-(leaf migemo
-  :ensure t
-  :require t
-  :custom
-  (migemo-command . "cmigemo")
-  (migemo-options . '("-q" "--emacs"))
-  (migemo-dictionary . "/usr/share/cmigemo/utf-8/migemo-dict")
-  (migemo-user-dictionary . nil)
-  (migemo-regex-dictionary . nil)
-  (migemo-coding-system . 'utf-8-unix)
-  :config
-  (migemo-init))
-
-(leaf ripgrep
-  :ensure t
-  :leaf-defer t
-  :bind (("M-s r" . ripgrep-regexp))
-  :config
-  (setq ripgrep-arguments '("-S")))
-
-;; 大文字・小文字を区別しないでサーチ（有効：t、無効：nil）
-(setq case-fold-search nil)
-
-;; インクリメント検索時に縦スクロールを有効化（有効：t、無効：nil）
-(setq isearch-allow-scroll nil)
-
-;; C-dで検索文字列を一文字削除
-(define-key isearch-mode-map (kbd "C-d") 'isearch-delete-char)
-
-;; C-yで検索文字列にヤンク貼り付け
-(define-key isearch-mode-map (kbd "C-y") 'isearch-yank-kill)
-
-;; C-eで検索文字列を編集
-(define-key isearch-mode-map (kbd "C-e") 'isearch-edit-string)
-
-;; Tabで検索文字列を補完
-(define-key isearch-mode-map (kbd "TAB") 'isearch-yank-word)
-
-;; C-gで検索を終了
-(define-key isearch-mode-map (kbd "C-g")
-  '(lambda()(interactive) (isearch-done)))
-
-;; 日本語の検索文字列をミニバッファに表示
-(define-key isearch-mode-map (kbd "<compend>")
-  '(lambda() (interactive) (isearch-update)))
-(define-key isearch-mode-map (kbd "<kanji>")
-  'isearch-toggle-input-method)
-(add-hook
- 'isearch-mode-hook
- '(lambda() (setq w32-ime-composition-window (minibuffer-window)))
- )
-(add-hook
- 'isearch-mode-end-hook
- '(lambda() (setq w32-ime-composition-window nil))
- )
-
-;; minibufferのアクティブ時、IMEを無効化
-(add-hook 'minibuffer-setup-hook
-          (lambda ()
-            (deactivate-input-method)))
 
 (setq comment-style 'extra-line)
 
